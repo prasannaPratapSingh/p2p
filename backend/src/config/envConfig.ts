5aa0318e7d7e63847100fa2bf9300e6de02c9fe6
Prasanna <prap.sin.25@gmail.com>
2026-05-24 04:33:39 +0530

    - Add database connection setup and environment variable validation

--- backend/src/config/envConfig.ts
+++ backend/src/config/envConfig.ts
@@ -12,16 +12,23 @@ if (!process.env.NODE_ENV) {
     process.exit(1);
 }
 
+if (!process.env.DB_URL) {
+    logger.error("DB_URL env variable does not loaded successfully!")
+    process.exit(1);
+}
+
 
 interface EnvConfig {
     PORT: Number,
-    NODE_ENV: String
+    NODE_ENV: String,
+    DB_URL: string
 }
 
 
 const envConfig: EnvConfig = {
     PORT: Number(process.env.PORT),
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    DB_URL: process.env.DB_URL
 }
 
 export default envConfig;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app.js";
 import envConfig from "./config/envConfig.js";
+import connectDB from "./infrastructure/database/mongoose.js";
 
 
 
-app.listen(envConfig.PORT, () => {
+app.listen(envConfig.PORT, async () => {
+    await connectDB();
     console.log(`Server sun raha hai ${envConfig.PORT} port par!`);
 })
