d96b40cc80fb7c747e2ddfc2b9ebdb517110e24f
Prasanna <prap.sin.25@gmail.com>
2026-05-25 10:20:52 +0530

    - Integrate the express-rate-limiter to prevent the api abuse. - create a rateLimiter.middleware. - create three rate limiters based on the strength - we have strict, moderate and general limiter based on it's windowMs & max perimeter.

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { validateRequest } from "../../middlewares/validateRequest.middleware.js";
 import { loginSchema, registerSchema } from "./auth.validation.js";
 import { login, logout, refreshToken, register } from "./auth.controller.js";
+import { strictAuthLimiter } from "../../middlewares/rateLimiter.middleware.js";
 
 const router = Router();
 
 router.post('/register', validateRequest(registerSchema), register);
-router.post('/login', validateRequest(loginSchema), login);
+router.post('/login', strictAuthLimiter, validateRequest(loginSchema), login);
 router.post('/refresh', refreshToken);
 router.post('/logout', logout);
 
