4c0c5db1661331eb0354fb9e6872e69c44983919
Prasanna <prap.sin.25@gmail.com>
2026-06-06 10:38:56 +0530

    - setup the socket in the backend completely.

---
+++ backend/src/config/socket.ts
@@ -0,0 +1,54 @@
+import { Server as HTTPServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import { logger } from './logger.js';
+
+let io: Server;
+
+
+// function to initialize the socket or upgrade out HTTP server...
+export const initSocket = (server: any) => {
+
+    io = new Server(server, {
+        cors: {
+            origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+            credentials: true,
+        },
+    });
+
+    io.on("connection", (socket: Socket) => {
+        // handhshake ya upgrade request ke time hi hum userId le lege 
+
+        const userId = socket.handshake?.query?.userId as String;
+
+        if (userId && userId !== 'undefined') {
+
+            const userRoom = `user${userId}`;
+
+            socket.join(userRoom);
+
+            logger.info(`📡 Synapse Node Joined Room: ${userRoom} (Socket ID: ${socket.id})`);
+        }
+        socket.on("disconnect", () => {
+            logger.info(`🛑 Socket Disconnected: ${socket.id}`);
+        })
+        return io;
+    })
+
+
+}
+
+// ab ek helper function banana hoga so that hume baar baar same boilerplate na likhni pade i.e room me message emit karne wala...
+
+export const sendRealTimeNotification = (recipentId: String, payload: any) => {
+    if (!io) {
+        logger.error("Socket.io server is not initialized. Cannot send real-time notification.");
+        return;
+    }
+
+    const targetRoom = `user_${recipentId}`;
+
+    io.to(targetRoom).emit("NEW_NOTIFICATION", payload);
+
+    logger.info(`⚡ Synaptic Room Push Delivered to: ${targetRoom}`);
+
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import app from "./app.js";
 import envConfig from "./config/envConfig.js";
+import { initSocket } from "./config/socket.js";
 import connectDB from "./infrastructure/database/mongoose.js";
 import connectRedis from "./infrastructure/redis/redis.js";
+import { createServer } from 'http';
+
+const httpServer = createServer(app);
+initSocket(httpServer);
 
 
-
-
-app.listen(envConfig.PORT, async () => {
+httpServer.listen(envConfig.PORT, async () => {
     await connectDB();
     await connectRedis();
     console.log(`Server is listening at ${envConfig.PORT} port 🔌`);
